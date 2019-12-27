647b93daab3aeca9a1d3e0dcab307724a6a622e0
Vipin Samleti <vipsamleti2@gmail.com>
2019-12-28 01:36:03 +0530

    Updates

---
+++ precache-manifest.9a115b70e9ec1fc4e20dd3d0ef1861c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a52831dc2a8482d6cc670a574e51018f",
+    "url": "/learn-react/index.html"
+  },
+  {
+    "revision": "21ff5d426dacb46e4bb0",
+    "url": "/learn-react/static/css/main.9876c1bc.chunk.css"
+  },
+  {
+    "revision": "b233c629ac64cdec7648",
+    "url": "/learn-react/static/js/2.62d11771.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/learn-react/static/js/2.62d11771.chunk.js.LICENSE"
+  },
+  {
+    "revision": "21ff5d426dacb46e4bb0",
+    "url": "/learn-react/static/js/main.4e95ff09.chunk.js"
+  },
+  {
+    "revision": "b5eafaeb5d4abda09579",
+    "url": "/learn-react/static/js/runtime-main.84284ae7.js"
+  }
+]);
