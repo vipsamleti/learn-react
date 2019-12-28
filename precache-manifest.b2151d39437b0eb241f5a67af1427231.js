c3cd3ed38c23ba6c952c5b02816ec3d03500678d
Vipin Samleti <vipsamleti2@gmail.com>
2019-12-28 11:31:00 +0530

    Updates

---
+++ precache-manifest.b2151d39437b0eb241f5a67af1427231.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aca3c0e79dbc3b101a5c8c120a5a3dda",
+    "url": "/search-web-series/index.html"
+  },
+  {
+    "revision": "4988ae296271e4f84e9d",
+    "url": "/search-web-series/static/css/main.9876c1bc.chunk.css"
+  },
+  {
+    "revision": "b233c629ac64cdec7648",
+    "url": "/search-web-series/static/js/2.62d11771.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/search-web-series/static/js/2.62d11771.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4988ae296271e4f84e9d",
+    "url": "/search-web-series/static/js/main.8944ff1a.chunk.js"
+  },
+  {
+    "revision": "664bc6ab7fedaa04f686",
+    "url": "/search-web-series/static/js/runtime-main.a80620a6.js"
+  }
+]);
