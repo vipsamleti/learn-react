e2b4bd53726854c155b69af5cac7c79af46aa4fa
Vipin Samleti <vipsamleti2@gmail.com>
2019-12-28 12:04:31 +0530

    changes

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { HashRouter, Route, Link } from "react-router-dom";
 import Main from '../main/Main'
 import './App.css';
 import 'whatwg-fetch';
@@ -8,15 +9,17 @@ class App extends Component {
   
   render(){
     return (
-      <div className="App">
-        <header className="App-header">
-          
-          <h1>ReactJS Application for searching Web-Series details..</h1>
+    
+          <div className="App">
+            <header className="App-header">
+              
+              <h1>ReactJS Application for searching Web-Series details..</h1>
 
-          <Main/>
+              <Main/>
 
-        </header>
-      </div>
+            </header>
+          </div>
+      
     );
   }
 }
